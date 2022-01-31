095da17a7604ad76061cb1e3588f5ca35413160f
ankit <ankitgc1@gmail.com>
2022-01-31 13:06:37 +0530

    Updates

---
+++ precache-manifest.16c78ce84060a2ebfd9e557f8e0f5366.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9408efd2be1de59385886a7cbca4aa13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "909ea9c025ccfa0ad63a",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "a4fb7b9c06d0ea8210f8",
+    "url": "/static/css/main.49ca4785.chunk.css"
+  },
+  {
+    "revision": "909ea9c025ccfa0ad63a",
+    "url": "/static/js/2.0cc0d070.chunk.js"
+  },
+  {
+    "revision": "90a099aa1d0c26961066026ab7d5e7a2",
+    "url": "/static/js/2.0cc0d070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f44f8202162cd6c4f1",
+    "url": "/static/js/3.e557942d.chunk.js"
+  },
+  {
+    "revision": "a4fb7b9c06d0ea8210f8",
+    "url": "/static/js/main.65f8f2a1.chunk.js"
+  },
+  {
+    "revision": "ec9ae744b3705d0b858f",
+    "url": "/static/js/runtime-main.e6ecb568.js"
+  },
+  {
+    "revision": "6aa6fbd1d76f72bbcd994f752e46263d",
+    "url": "/static/media/Artist.6aa6fbd1.jpeg"
+  },
+  {
+    "revision": "0ee7d34e6d00738dd1dde008049c4487",
+    "url": "/static/media/Developer.0ee7d34e.jpeg"
+  },
+  {
+    "revision": "234ba0c2d0f85f15257edfb13298233c",
+    "url": "/static/media/Programmer.234ba0c2.jpeg"
+  },
+  {
+    "revision": "e4484571b47da9c5020c8f8e3ace37e2",
+    "url": "/static/media/bg.e4484571.png"
+  },
+  {
+    "revision": "3617e6657b8f1d4e4bd4cebc659b07ec",
+    "url": "/static/media/cf.3617e665.jpeg"
+  },
+  {
+    "revision": "9b12e0e6f2129e50d9526469e7cbcad1",
+    "url": "/static/media/gif.9b12e0e6.gif"
+  },
+  {
+    "revision": "aad7ec05e72fac004db815061658c406",
+    "url": "/static/media/img1.aad7ec05.jpeg"
+  },
+  {
+    "revision": "bfffb339564663c05c816f905a418149",
+    "url": "/static/media/img2.bfffb339.jpeg"
+  },
+  {
+    "revision": "ff65d858597c7aa0938f09ec64f1dde2",
+    "url": "/static/media/img3.ff65d858.jpeg"
+  },
+  {
+    "revision": "f5db1946b471eb4da5dba98f8ff56bd4",
+    "url": "/static/media/img4.f5db1946.jpeg"
+  },
+  {
+    "revision": "ea9393a63b02bd9f1fc118c6ed6fe326",
+    "url": "/static/media/logo.ea9393a6.png"
+  },
+  {
+    "revision": "716b9ef14dfabe349c2fdcffadee3bd0",
+    "url": "/static/media/mrscf.716b9ef1.jpeg"
+  },
+  {
+    "revision": "0911f39d6f15d65a68a30c8afa8a869e",
+    "url": "/static/media/twitter.0911f39d.png"
+  }
+]);
